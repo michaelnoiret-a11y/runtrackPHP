cfc6311c0c9f48bcb58ad12f51806e1982aea4b8
michael.noiret <michael.noiret@laplateforme.io>
2026-01-05 17:02:21 +0100

    Job1

---
+++ Jour1/Job1/index.php
@@ -0,0 +1,23 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . " " ;
+$val = 6;
+echo "<br>";
+echo $val;
+$val += 4;
+echo "<br>";
+echo $val;
+echo "<br>";
+$myBool = True;
+
+echo gettype($myBool);
+echo $myBool;
+var_dump($myBool);
+echo "<br>";
+$myBool = false;
+echo gettype($myBool);
+echo $myBool;
+var_dump($myBool);
+?>
