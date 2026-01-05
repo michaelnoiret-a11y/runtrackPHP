b19e0d2d5aff0f5699552fa1ede41cee4efe33e4
michael.noiret <michael.noiret@laplateforme.io>
2026-01-05 15:22:17 +0100

    Ajout Job2

---
+++ Jour1/Job2/index.php
@@ -0,0 +1,8 @@
+
+<?php
+echo "Hello LaPlateforme!"; // Afficher cette chaîne de caractère
+echo "<br>";
+print "Hello LaPlateforme ! "; /* Afficher cette chaîne de caractère */
+echo "<br>"
+# commentaire
+?>
