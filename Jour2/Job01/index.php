1ff2361ec2039ca7aab6d171afee28b29c2f13c5
michael.noiret <michael.noiret@laplateforme.io>
2026-01-07 14:49:35 +0100

    Ajout Job01

---
+++ Jour2/Job01/index.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($i = 0 ; $i <= 1337 ; $i++){
+    if ($i === 42){
+        echo "<br>";
+        echo "<strong>" . "<u>" . $i . "</strong>" . "</u>";
+    } else {
+        echo "<br>";
+        echo $i ;
+    }
+    
+}
+
+?>
