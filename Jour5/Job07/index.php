a4549a38a834fc975c48b22d187fa1a80b33af77
michael.noiret <michael.noiret@laplateforme.io>
2026-01-12 23:04:41 +0100

    Ajout Job07

---
+++ Jour5/Job07/index.php
@@ -0,0 +1,27 @@
+<?php
+$categories = [
+    ""
+]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <form action="" method="get">
+            <select name="choix" id="choix"></select>
+            <option value="">-- Sélectionnez --</option>
+            <input type="text" name="str" placeholder="mots">
+            <input type="submit" name="submit" value="envoyez">
+        </form>
+    </main>
+</body>
+</html>
