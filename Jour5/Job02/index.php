ee0fb99dd3c8f0f2ac435c0ff6381b41923d733b
michael.noiret <michael.noiret@laplateforme.io>
2026-01-12 22:59:22 +0100

    Ajout Job02

---
+++ Jour5/Job02/index.php
@@ -0,0 +1,54 @@
+<?php
+// $jour = FALSE;
+// function bonjour($jour){
+    
+//     if ($jour ) {
+//         echo "Bonjour";
+// }
+//     else {
+//         echo "Bonsoir";
+// }
+// }
+
+// bonjour($jour);
+
+
+function bonjour($jour){
+    
+    if ($jour == True ) {
+        echo "Bonjour";
+}
+    else {
+        echo "Bonsoir";
+}
+}
+
+ bonjour(False);
+
+// if (isset($_GET["submit"])){
+//     $jour = $_GET["jour"];
+//     echo (" jour :". $jour);
+//     bonjour($jour);
+// }
+
+//?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     <header>
+
+     </header>
+     <main>
+         </form>
+         <form action="" method="get">
+             <input type="text" name="jour" placeholder="jour">
+             <input type="submit" name="submit"value="envoyez">
+         </form>
+     </main>
+ </body>
+ </html> -->
