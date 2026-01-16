c4da30ab7ea3705fcf7d8ed4cfbf431f17c0c4e0
michael.noiret <michael.noiret@laplateforme.io>
2026-01-16 09:56:39 +0100

    Ajout Job02

---
+++ Jour9/Job02/index.php
@@ -0,0 +1,39 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=jour08;charset=utf8","root", "");
+$sql = "SELECT nom, capacite FROM salles";
+$query = $pdo->prepare($sql);
+$query->execute();
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+# var_dump($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <?php
+    echo "<table border='1'>";
+    echo" <tr>
+      <th scope='col'>nom</th>
+      <th scope='col'>capacite</th>
+    </tr>";
+    
+    foreach ($result as $value) {
+        echo "<tr>";
+        echo " <td>". $value["nom"] ."</td>";
+        echo " <td>". $value["capacite"] ."</td>";
+        echo "</tr>";
+    }
+    echo"</table>";
+    ?>
+    </main>
+</body>
+</html>
